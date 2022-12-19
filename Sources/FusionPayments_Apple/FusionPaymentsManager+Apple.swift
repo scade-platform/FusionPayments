88e45cf240de184d858c4394e7be4233af050ff6
Vedant Jha <6vedant@gmail.com>
2022-12-19 16:50:50 +0530

    PaymentError added

--- Sources/FusionPayments_Apple/FusionPaymentsManager+Apple.swift
+++ Sources/FusionPayments_Apple/FusionPaymentsManager+Apple.swift
@@ -16,81 +16,78 @@ public class FusionPaymentsManager: NSObject, FusionPaymentsManagerProtocol {
         
     }
     
-    var completionHandler: ((PaymentStatus) -> Void)?
+    var completionHandler: ((PaymentStatus, PaymentError?) -> Void)?
     
     
     
-    public func initiatePayment( paymentRequest: PaymentRequest, paymentStatus: @escaping (PaymentStatus) -> Void ) {
-       
+    public func initiatePayment( paymentRequest: PaymentRequest, paymentStatus: @escaping (PaymentStatus, PaymentError?) -> Void ) {
         
-            let paymentItem = PKPaymentSummaryItem.init(
-                label: "lable", amount: NSDecimalNumber(value: 33))
-       
-            let paymentNetworks = [PKPaymentNetwork.amex, .discover, .masterCard, .visa]
-
-            if PKPaymentAuthorizationViewController.canMakePayments(usingNetworks: paymentNetworks) {
-              let request = PKPaymentRequest()
-              request.currencyCode = "USD"  // 1
-              request.countryCode = "US"  // 2
-              request.merchantIdentifier = "merchant.com.vedant.fusionpayments"  // 3
-              request.merchantCapabilities = PKMerchantCapability.capability3DS  // 4
-              request.supportedNetworks = paymentNetworks  // 5
-              request.paymentSummaryItems = [paymentItem]  // 6
-
-              guard let paymentVC = PKPaymentAuthorizationViewController(paymentRequest: request) else {
+        
+        let paymentItem = PKPaymentSummaryItem.init(
+            label: "lable", amount: NSDecimalNumber(value: 33))
+        
+        let paymentNetworks = [PKPaymentNetwork.amex, .discover, .masterCard, .visa]
+        
+        if PKPaymentAuthorizationViewController.canMakePayments(usingNetworks: paymentNetworks) {
+            let request = PKPaymentRequest()
+            request.currencyCode = "USD"  // 1
+            request.countryCode = "US"  // 2
+            request.merchantIdentifier = "merchant.com.vedant.fusionpayments"  // 3
+            request.merchantCapabilities = PKMerchantCapability.capability3DS  // 4
+            request.supportedNetworks = paymentNetworks  // 5
+            request.paymentSummaryItems = [paymentItem]  // 6
+            
+            guard let paymentVC = PKPaymentAuthorizationViewController(paymentRequest: request) else {
                 //   displayDefaultAlert(title: "Error", message: "Unable to present Apple Pay authorization.")
                 print("unable to present apple pay authorization: error")
                 return
-              }
-              paymentVC.delegate = self
-                #if os(iOS)
-                UIApplication.shared.delegate?.window??.rootViewController?.present(
-                paymentVC, animated: true, completion: nil)
-                #endif
-                
-            } else {
-              // displayDefaultAlert(title: "Error", message: "Unable to make Apple Pay transaction.")
-              print("error: unable to make apple pay transaction")
             }
+            paymentVC.delegate = self
+#if os(iOS)
+            UIApplication.shared.delegate?.window??.rootViewController?.present(
+                paymentVC, animated: true, completion: nil)
+#endif
+            
+        } else {
+            // displayDefaultAlert(title: "Error", message: "Unable to make Apple Pay transaction.")
+            print("error: unable to make apple pay transaction")
+            completionHandler?(.SUCCESS, .UNKNOWN_ERROR)
+        }
         
         completionHandler = paymentStatus
         
     }
     
-
+    
 }
 
 @available(macOS 11.0, *)
 extension FusionPaymentsManager:   PKPaymentAuthorizationViewControllerDelegate {
     public func paymentAuthorizationViewControllerDidFinish(
         _ controller: PKPaymentAuthorizationViewController
-      ) {
+    ) {
         //controller.dismiss(animated: true, completion: nil)
         //dismiss(animated: true, completion: nil)
         print("payment finished")
 #if os(iOS)
-          UIApplication.shared.delegate?.window??.rootViewController?.dismiss(
+        UIApplication.shared.delegate?.window??.rootViewController?.dismiss(
             animated: true, completion: nil)
-
-          completionHandler?(.FAILED)
-
-          #endif
-      }
+        
+        completionHandler?(.FAILED, nil)
+        
+#endif
+    }
     
-      public func paymentAuthorizationViewController(
+    public func paymentAuthorizationViewController(
         _ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment,
         handler completion: @escaping (PKPaymentAuthorizationResult) -> Void
-      ) {
+    ) {
         completion(PKPaymentAuthorizationResult(status: .success, errors: nil))
-        // displayDefaultAlert(title: "Success!", message: "The Apple Pay transaction was complete.")
         print(payment)
-        print(Unmanaged.passUnretained(payment).toOpaque())
-        print(Unmanaged.passUnretained(payment.token).toOpaque())
-
-        // print(completion)
+        
         print("payment done")
-          completionHandler?(.SUCCESS)
-
-      }
-
+        completionHandler?(.SUCCESS, nil)
+        
+    }
+    
 }
--- Sources/FusionPayments_Common/FusionPaymentsManager.swift
+++ Sources/FusionPayments_Common/FusionPaymentsManager.swift
@@ -10,7 +10,7 @@ import Foundation
 public protocol FusionPaymentsManagerProtocol  {
     init()
     
-    func initiatePayment( paymentRequest: PaymentRequest, paymentStatus: @escaping (PaymentStatus) -> Void )
+    func initiatePayment( paymentRequest: PaymentRequest, paymentStatus: @escaping (PaymentStatus, PaymentError?) -> Void )
 }
 
 public enum PaymentStatus {
@@ -18,6 +18,42 @@ public enum PaymentStatus {
     
     case FAILED
 }
+
+public enum PaymentError {
+    
+    case BILLING_CONTACT_INVALID_ERROR
+    
+    case SHIPPING_CONTACT_INVALID_ERROR
+    
+    case SHIPPING_CONTACT_INSERVICEABLE_ERROR
+    
+    case NETWORK_ERROR
+    
+    case UNKNOWN_ERROR
+    
+    public var description:String {
+                switch self {
+                
+                case .BILLING_CONTACT_INVALID_ERROR:
+                    return "Biometry is not available in the device."
+                    
+                case .SHIPPING_CONTACT_INVALID_ERROR:
+                    return "Biometry is locked because there were too many failed attempts."
+                    
+                case .SHIPPING_CONTACT_INSERVICEABLE_ERROR:
+                    return "The user has no enrolled biometric identities."
+                    
+                case .NETWORK_ERROR:
+                    return "The device supports biometry only using a removable accessory, but the paired accessory isn’t connected."
+                    
+                case .UNKNOWN_ERROR:
+                    return "The user tapped the cancel button in the authentication dialog."
+                    
+                }
+            }
+}
+
+
 
 
 
